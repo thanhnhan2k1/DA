9ba671d8f28a75579a66632feca7fd35bb9c5785
thanhnhan2k1 <92383193+thanhnhan2k1@users.noreply.github.com>
2023-10-25 01:17:51 +0700

    Update BlogController.java

--- src/main/java/com/example/controller/BlogController.java
+++ src/main/java/com/example/controller/BlogController.java
@@ -26,8 +26,8 @@ import jakarta.servlet.http.HttpSession;
 @RequestMapping("/blog")
 public class BlogController {
 	private RestTemplate rest=new RestTemplate();
-	private String url="http://localhost:8082";
-	//private String url="https://da-server2-production.up.railway.app";
+	//private String url="http://localhost:8082";
+	private String url="https://server-production-004b.up.railway.app";
 	@GetMapping
 	private String getString(HttpSession session,
 			@RequestParam(name="pageNum", defaultValue = "1", required = false) int pageNum,
